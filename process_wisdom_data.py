448342572c0e57510e4158931eeb19455ed33850
Skoomar <umar_farooqi@hotmail.co.uk>
2021-04-08 02:47:50 +0100

    ok now the merge works

--- process_wisdom_data.py
+++ process_wisdom_data.py
@@ -28,21 +28,6 @@ def read_data(file_path):
     return data
 
 
-# def match_records(match_label, arr1, arr2):
-#     for i in arr1.:
-#         print(i[match_label])
-#         # if arr1[match_label] in arr2[match_label]:
-#         #     arr1[match_label]
-#         # elif arr1[match_label] not in arr2[match_label]:
-
-#
-# def match_data_by_timestamp(accel, gyro):
-#     print(type(accel))
-#     if len(accel) > len(gyro):
-#         return match_records('timestamp', accel, gyro)
-#     else:
-#         return match_records('timestamp', gyro, accel)
-
 def match_accel_and_gyro_data(accel, gyro, device):
     merged_data = pd.merge(accel, gyro, how='inner', on=['user-id', 'activity', 'timestamp'])
     merged_data.rename(columns={'x-axis_x': 'x-axis_accel_' + device,
@@ -56,10 +41,6 @@ def match_accel_and_gyro_data(accel, gyro, device):
     return merged_data
 
 
-def manual_merge(d1, d2):
-    pass
-
-
 def merge_phone_watch_data(phone, watch):
     merged_data = pd.DataFrame(
         columns=['user-id', 'activity', 'timestamp', 'x-axis_accel_phone', 'y-axis_accel_phone', 'z-axis_accel_phone',
@@ -69,16 +50,18 @@ def merge_phone_watch_data(phone, watch):
 
     activity_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S']
     for a_id in activity_ids:
-        phone_subset = phone.loc[phone['activity'] == a_id]
-        watch_subset = watch.loc[watch['activity'] == a_id]
+        # reset indexes to start from 0 so data from both dataframes is properly lined up
+        phone_subset = phone.loc[phone['activity'] == a_id].reset_index(drop=True)
+        watch_subset = watch.loc[watch['activity'] == a_id].reset_index(drop=True)
         if (not phone_subset.empty) and (not watch_subset.empty):
-            phone_subset[['x-axis_accel_watch', 'y-axis_accel_watch', 'z-axis_accel_watch',
-                          'x-axis_gyro_watch', 'y-axis_gyro_watch', 'z-axis_gyro_watch']
-            ] = watch_subset[['x-axis_accel_watch', 'y-axis_accel_watch', 'z-axis_accel_watch',
-                              'x-axis_gyro_watch', 'y-axis_gyro_watch', 'z-axis_gyro_watch']]
+            phone_subset['x-axis_accel_watch'] = watch_subset['x-axis_accel_watch']
+            phone_subset['y-axis_accel_watch'] = watch_subset['y-axis_accel_watch']
+            phone_subset['z-axis_accel_watch'] = watch_subset['z-axis_accel_watch']
+            phone_subset['x-axis_gyro_watch'] = watch_subset['x-axis_gyro_watch']
+            phone_subset['y-axis_gyro_watch'] = watch_subset['y-axis_gyro_watch']
+            phone_subset['z-axis_gyro_watch'] = watch_subset['z-axis_gyro_watch']
             merged_data = merged_data.append(phone_subset, ignore_index=True)
     merged_data.dropna(axis=0, how='any', inplace=True)
-
     return merged_data
 
 
@@ -90,8 +73,8 @@ watch_gyro = read_data('wisdm-dataset/raw/watch/gyro/data_1600_gyro_watch.txt')
 phone_merge = match_accel_and_gyro_data(phone_accel, phone_gyro, 'phone')
 watch_merge = match_accel_and_gyro_data(watch_accel, watch_gyro, 'watch')
 # pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 # pd.set_option('display.max_colwidth', -1)
 complete_data = merge_phone_watch_data(phone_merge, watch_merge)
 # print(complete_data)
@@ -105,7 +88,4 @@ for i in activity_ids:
 print(len(phone_merge))
 print(len(watch_merge))
 print(len(complete_data))
-
-# phone_watch_merge = merge_phone_watch_data(phone_merge, watch_merge)
-# phone_watch_merge.to_csv('phone_watch_merge.txt')
-# print(phone_watch_merge)
+complete_data.to_csv('1600_merged_data.txt')
