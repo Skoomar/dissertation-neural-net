443b1126bea16b697f5f40b4bdb1e5a64f5480d8
Skoomar <umar_farooqi@hotmail.co.uk>
2021-05-08 20:32:32 +0100

    individual and conv layers done i think

--- myDeepSense.py
+++ myDeepSense.py
@@ -1,52 +1,92 @@
+import tensorflow as tf
 from tensorflow.keras import models, layers
 
 
-def individual_conv_layers(input_layer, filter_size, kernel_size):
-    conv = layers.Conv1D(filter_size, kernel_size)(input_layer)
-    conv = layers.BatchNormalization()(conv)
-    conv = layers.Activation('relu')(conv)
-    conv = layers.Dropout(0.2)(conv)
+def individual_conv_layers(input_layer):
+    num_filters = 64
+    kernel_size1 = 18
+    conv1 = layers.Conv1D(num_filters, kernel_size1)(input_layer)
+    conv1 = layers.BatchNormalization()(conv1)
+    conv1 = layers.Activation('relu')(conv1)
+    conv1 = layers.Dropout(0.2)(conv1)
     # accel_conv = layers.MaxPooling1D(pool_size=2)(accel_conv)
-    conv = layers.Flatten()(conv)
-    return conv
+    # conv1 = layers.Flatten()(conv1)
+
+    kernel_size2 = 3
+    conv2 = layers.Conv1D(num_filters, kernel_size2)(conv1)
+    conv2 = layers.BatchNormalization()(conv2)
+    conv2 = layers.Activation('relu')(conv2)
+    conv2 = layers.Dropout(0.2)(conv2)
+    # conv2 = layers.Flatten()(conv2)
+
+    conv3 = layers.Conv1D(num_filters, kernel_size2)(conv2)
+    conv3 = layers.BatchNormalization()(conv3)
+    conv3 = layers.Activation('relu')(conv3)
+
+    # out = layers.Flatten()(conv3)
+    conv3_shape = conv3.get_shape()
+    out = tf.reshape(conv3, [-1, conv3_shape[1], 1, conv3_shape[2]])
+    return out
 
 
 def uDeepSense(input_shape=(80, 3), num_classes=18):
-    filter_size = 64
-    kernel_size = 12
 
     ap = layers.Input(shape=input_shape)
     gp = layers.Input(shape=input_shape)
     aw = layers.Input(shape=input_shape)
     gw = layers.Input(shape=input_shape)
 
-    accel_conv = layers.Conv1D(filter_size, kernel_size)(ap)
-    accel_conv = layers.BatchNormalization()(accel_conv)
-    accel_conv = layers.Activation('relu')(accel_conv)
-    accel_conv = layers.Dropout(0.2)(accel_conv)
-    # accel_conv = layers.MaxPooling1D(pool_size=2)(accel_conv)
-    accel_conv = layers.Flatten()(accel_conv)
+    # accel_phone_conv = layers.Conv1D(filter_size, kernel_size)(ap)
+    # accel_phone_conv = layers.BatchNormalization()(accel_phone_conv)
+    # accel_phone_conv = layers.Activation('relu')(accel_phone_conv)
+    # accel_phone_conv = layers.Dropout(0.2)(accel_phone_conv)
+    # # accel_phone_conv = layers.MaxPooling1D(pool_size=2)(accel_phone_conv)
+    # accel_phone_conv = layers.Flatten()(accel_phone_conv)
+    #
+    # gyro_phone_conv = layers.Conv1D(filter_size, kernel_size)(gp)
+    # gyro_phone_conv = layers.BatchNormalization()(gyro_phone_conv)
+    # gyro_phone_conv = layers.Activation('relu')(gyro_phone_conv)
+    # gyro_phone_conv = layers.Dropout(0.2)(gyro_phone_conv)
+    # # gyro_phone_conv = layers.MaxPooling1D(pool_size=2)(gyro_phone_conv)
+    # gyro_phone_conv = layers.Flatten()(gyro_phone_conv)
+    #
+    accel_phone_conv = individual_conv_layers(ap)
+    gyro_phone_conv = individual_conv_layers(gp)
+    accel_watch_conv = individual_conv_layers(aw)
+    gyro_watch_conv = individual_conv_layers(gw)
 
-    gyro_conv = layers.Conv1D(filter_size, kernel_size)(gp)
-    gyro_conv = layers.BatchNormalization()(gyro_conv)
-    gyro_conv = layers.Activation('relu')(gyro_conv)
-    gyro_conv = layers.Dropout(0.2)(gyro_conv)
-    # gyro_conv = layers.MaxPooling1D(pool_size=2)(gyro_conv)
-    gyro_conv = layers.Flatten()(gyro_conv)
-    # accel_conv = individual_conv_layers(ap, filter_size, kernel_size)
-    # gyro_conv = individual_conv_layers(gp, filter_size, kernel_size)
+    individual_output = layers.concatenate([accel_phone_conv, gyro_phone_conv, accel_watch_conv, gyro_watch_conv], 2)
+    individual_output = layers.Dropout(0.2)(individual_output)
+    print(individual_output.get_shape())
 
-    merge_layer = layers.concatenate([accel_conv, gyro_conv])
-    out = layers.Dense(num_classes, activation='softmax')(merge_layer)
+    merge_conv1 = layers.Conv2D(64, [2, 16], padding='same')(individual_output)
+    merge_conv1 = layers.BatchNormalization()(merge_conv1)
+    merge_conv1 = layers.Activation('relu')(merge_conv1)
+    merge_conv1 = layers.Dropout(0.2)(merge_conv1)
 
-    model = models.Model(inputs=[ap, gp], outputs=[out])
+    merge_conv2 = layers.Conv2D(64, [2, 12], padding='same')(merge_conv1)
+    merge_conv2 = layers.BatchNormalization()(merge_conv2)
+    merge_conv2 = layers.Activation('relu')(merge_conv2)
+    merge_conv2 = layers.Dropout(0.2)(merge_conv2)
+
+    merge_conv3 = layers.Conv2D(64, [2, 8], padding='same')(merge_conv2)
+    merge_conv3 = layers.BatchNormalization()(merge_conv3)
+    merge_conv3 = layers.Activation('relu')(merge_conv3)
+
+    merge_conv3_shape = merge_conv3.get_shape()
+    merge_output = tf.reshape(merge_conv3, [-1, merge_conv3_shape[1], merge_conv3_shape[2]*merge_conv3_shape[3]])
+    print("merge_output:", merge_output.get_shape())
+
+    output = layers.Dense(num_classes, activation='softmax')(merge_conv3)
+
+    model = models.Model(inputs=[ap, gp, aw, gw], outputs=[output])
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
     return model
 
 
 def train(model, train_ap, train_gp, train_aw, train_gw, train_labels, batch_size, epochs, verbose=1):
-    model.fit([train_ap, train_gp], train_labels,
+    model.fit([train_ap, train_gp, train_aw, train_gw], train_labels,
               batch_size=batch_size,
               epochs=epochs,
               # callbacks=callbacks_list,
@@ -56,5 +96,5 @@ def train(model, train_ap, train_gp, train_aw, train_gw, train_labels, batch_siz
 
 
 def evaluate(model, test_ap, test_gp, test_aw, test_gw, test_labels, verbose=2):
-    test_loss, test_accuracy = model.evaluate([test_ap, test_gp], test_labels, verbose=verbose)
+    test_loss, test_accuracy = model.evaluate([test_ap, test_gp, test_aw, test_gw], test_labels, verbose=verbose)
     return test_accuracy
